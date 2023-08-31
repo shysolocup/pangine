e62e619b1c10a4580657196daaf2b0c8800101e0
paige <88659700+paigeroid@users.noreply.github.com>
2023-08-31 15:04:25 -0500

    Create Event.js

---
+++ src/classes/Event.js
@@ -0,0 +1,27 @@
+const { Soup } = require('stews');
+
+class Event {
+    constructor() {
+        this.data = new Soup(Array);
+
+        return new Proxy(this, {
+            get(target, prop) {
+                if (Object.getOwnPropertyNames(Event.prototype).includes(prop) || target[prop]) { // if it's a function or main thing
+                    return target[prop];
+                }
+                else {
+                    return target.data[prop];
+                }
+            }
+        })
+    }
+
+
+    fire(/**/) {
+        this.data.forEach( (v) => {
+            v(...arguments);
+        });
+    }
+}
+
+module.exports = Event;
